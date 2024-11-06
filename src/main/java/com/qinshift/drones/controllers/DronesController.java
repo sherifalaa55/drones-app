41b20f26a1dcbebc20877d5a5861be30f6cb11e0
Sherif Alaa <sherif.alaa55@gmail.com>
2024-11-06 23:04:32 +0200

    Impl register drone api, added controller method

--- src/main/java/com/qinshift/drones/controllers/DronesController.java
+++ src/main/java/com/qinshift/drones/controllers/DronesController.java
@@ -1,10 +1,13 @@
 package com.qinshift.drones.controllers;
 
 import com.qinshift.drones.domain.Drone;
+import com.qinshift.drones.dto.DroneDto;
 import com.qinshift.drones.enums.DroneState;
 import com.qinshift.drones.repositories.DroneRepository;
+import com.qinshift.drones.services.DroneService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +20,7 @@ import java.util.List;
 @RequestMapping("/api/v1/drones")
 public class DronesController {
 
+    private final DroneService droneService;
     @Autowired
     private final DroneRepository droneRepo;
 
@@ -25,5 +29,11 @@ public class DronesController {
         List<Drone> drones = droneRepo.findAllByState(DroneState.IDLE);
 
         return ResponseEntity.ok(drones);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Drone> resgisterDrone(@RequestBody @Validated DroneDto droneDto) throws Exception {
+        Drone drone = droneService.register(droneDto);
+        return new ResponseEntity<Drone>(drone, HttpStatus.CREATED);
     }
 }
